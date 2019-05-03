2c67271d85e7ce6520acbd0d7980fda182ac5c4f
doradoe1 <doradoe1@outlook.com>
2019-05-03 11:55:21 -0500

    another commit

--- p1script.sh
+++ p1script.sh
@@ -5,18 +5,27 @@ read -p "Enter the resource group name: " resourcegroup
 read -p "Enter the disk name: " diskname
 read -p "Enter the os type (Linux or Windows): " os
 read -p "Enter the name of the vm: " vmname
-read -p "Enter a name for the availability set: " as
+read -p "Enter a name for the scale set: " ss
 read -p "Enter the image (UbuntuLTS commonly used): " image
 read -p "Enter the size (Standard_B2s commonly used): " size
-read -p "Enter the size (read in gb) for the disk: "  sizegb
+read -p "Enter the size of the disk: "  sizegb
 read -p "Enter the custom data txt file: " cloudinit
 
 #--------------------------------------##Dummyproffing the new inputs:##--------------------------------------#
+##Verify for valid availability set:##
+verifyas=$(az vm availability-set list \
+| grep -E $as)
+if [ -z $verifyas ]; then
+    exit 0
+else
+    echo "Invalid/Existent availability-set. Use a different name and try again" 1>&2
+fi
+
 ##Verify for valid Resource Group:##
 verifyrg=$(az group exists \
     --name $resourcegroup \
 | grep -E true)
-if [ $verifyvrg=true ]; then
+if [ $verifyvrg=true ]; then    
     exit 0
 else
     echo "Invalid/non-existent Resource group. Please check your input and try again." 1>&2
@@ -46,12 +55,13 @@ else
 fi
 
 #-------------------------------------##Create the VM's and the Avaib. Set:##----------------------------------#
-##Create the availability set:##
-az vm availability-set create \
---resource-group $resourcegroup \
---name $as \
---platform-fault-domain-count 2 \
---platform-update-domain-count 2
+##Create the scale set:##
+az vmss create \
+  --resource-group $resourcegroup \
+  --name $ss \
+  --image $image \
+  --upgrade-policy-mode automatic \
+  --generate-ssh-keys
 
 ##Creating the Disk:##
 for i in `seq 1 2 3 4`; do
