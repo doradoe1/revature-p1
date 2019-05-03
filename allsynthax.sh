2632f07125476908d97185da6f50e09f49f8215e
doradoe1 <doradoe1@outlook.com>
2019-05-02 22:46:03 -0500

    commit

--- allsynthax.sh
+++ allsynthax.sh
@@ -13,71 +13,71 @@ read -p "Enter the image (UbuntuLTS commonly used): " image
 read -p "Enter the size (Standard_B2s commonly used): " size
 read -p "Enter the custom data script name:" cloudinit
 osDiskId=$(az vm show \
--g $resourcegroup \
--n $vmname \
---query "storageProfile.osDisk.managedDisk.id" \
+    -g $resourcegroup \
+    -n $vmname \
+    --query "storageProfile.osDisk.managedDisk.id" \
 -o tsv)
 ssname=$diskname.snapshot
 
 #--------------------------------------##Dummyproffing the new inputs:##--------------------------------------#
 ##Verify that the disk name is not taken:##
 verifydisk=$(az disk list \
---resource-group $resourcegroup \
---query [].name \
+    --resource-group $resourcegroup \
+    --query [].name \
 | grep -E $diskname)
 if [ -z $verifydisk ]; then
-exit 0
+    exit 0
 else
-echo "Disk name already in use. Please choose another name for the disk and try again." 1>&2
-exit 1
+    echo "Disk name already in use. Please choose another name for the disk and try again." 1>&2
+    exit 1
 fi
 
 ##Verify for valid VM:##
 verifyvm=$(az vm list \
---resource-group $resourcegroup \
--d \
---query [].name \
+    --resource-group $resourcegroup \
+    -d \
+    --query [].name \
 | grep -E $vmname)
 if [ -z $verifyvm ]; then
-echo "Invalid/non-existent VM. Please check your input and try again."
-1>&2
+    echo "Invalid/non-existent VM. Please check your input and try again."
+    1>&2
 else
-exit 0
+    exit 0
 fi
 
 ##Verify for valid Resource Group:##
 verifyrg=$(az group exists \
---name $resourcegroup \
+    --name $resourcegroup \
 | grep -E true)
 if [ $verifyvrg=true ]; then
-exit 0
+    exit 0
 else
-echo "Invalid/non-existent Resource group. Please check your input and try again."
-exit 1
+    echo "Invalid/non-existent Resource group. Please check your input and try again."
+    exit 1
 fi
 
 ##Verify for valid Image name:##
 verifyimage=$(az image list \
---resource-group $resourcegroup \
---query [].name \
+    --resource-group $resourcegroup \
+    --query [].name \
 | grep -E $imagename)
 if [ -z $verifyimage ]; then
-exit 0
+    exit 0
 else
-echo "Image name in use. Please choose another name for the image."
-exit 1
-fi 
+    echo "Image name in use. Please choose another name for the image."
+    exit 1
+fi
 
 ##Verify that the snapshot does not exist:##
 verifyss=$(az snapshot list \
---resource-group $resourcegroup \
---query [].name \
+    --resource-group $resourcegroup \
+    --query [].name \
 | grep -E $ssname)
 if [ -z $verifyss ]; then
-exit 0
+    exit 0
 else
-echo "Snapshot name already in use. Enter a different name and try again" 1>&2
-exit 1
+    echo "Snapshot name already in use. Enter a different name and try again" 1>&2
+    exit 1
 fi
 
 #----------------------------------------------##Commamd Synthax:##----------------------------------------------#
@@ -90,7 +90,7 @@ az vm open-port \
 ##Creating the Disk:##
 az disk create \
 --name $diskname \
---resource-group $resourcegroup \         
+--resource-group $resourcegroup \
 --os-type $os \
 --size-gb $sizegb
 
@@ -114,7 +114,7 @@ az vm disk detach \
 
 ##Creating the snapshot:##
 az snapshot create \
---name $ssname 
+--name $ssname
 --resource-group $resourcegroup \
 --source $osDiskId
 
@@ -129,7 +129,7 @@ az vm create \
 --name $vmname \
 --image $image \
 --size $size \
---generate-ssh-keys 
+--generate-ssh-keys
 
 ##Create VM with custom data:##
 az vm create \
--- vm_scripts/availabilityset.sh
+++ vm_scripts/availabilityset.sh
@@ -6,31 +6,66 @@ read -p "Enter the name of the vm: " vmname
 read -p "Enter a name for the availability set: " as
 read -p "Enter the image (UbuntuLTS commonly used): " image
 read -p "Enter the size (Standard_B2s commonly used): " size
+read -p "Enter the custom data txt file: " cloudinit
 
 #--------------------------------------##Dummyproffing the new inputs:##--------------------------------------#
-
 ##Verify for valid Resource Group:##
 verifyrg=$(az group exists \
---name $resourcegroup \
+    --name $resourcegroup \
 | grep -E true)
 if [ $verifyvrg=true ]; then
-exit 0
+    exit 0
 else
-echo "Invalid/non-existent Resource group. Please check your input and try again."
-exit 1
+    echo "Invalid/non-existent Resource group. Please check your input and try again."
+    exit 1
 fi
 
+##Verify for valid VM name:##
+verifyvm=$(az vm list \
+    --resource-group $resourcegroup \
+    -d \
+    --query [].name \
+| grep -E $vmname)
+if [ -z $verifyvm ]; then
+    exit 0
+else
+    echo "VM name already in use. Please check your input and try again."
+    1>&2
+fi
+
+#-------------------------------------##Create the VM's and the Avaib. Set:##----------------------------------#
+##Create the availability set:##
 az vm availability-set create \
 --resource-group $resourcegroup \
 --name $as \
 --platform-fault-domain-count 2 \
 --platform-update-domain-count 2
 
-for i in `seq 1 2 3`; do
-   az vm create \
-     --resource-group $resourcegroup \
-     --name $vmname$i \
-     --availability-set $as \
-     --size $size \
-     --image $image
+##Creating the Disk:##
+for i in `seq 1 2 3 4`; do
+    az disk create \
+    --name $diskname$i \
+    --resource-group $resourcegroup \
+    --os-type $os \
+    --size-gb $sizegb
+done
+
+##Create the VM:##
+for i in `seq 1 2 3 4`; do
+    az vm create \
+    --resource-group $resourcegroup \
+    --name $vmname$i \
+    --availability-set $as \
+    --size $size \
+    --image $image \
+    --disk-attach $diskname$i \
+    --custom-data $cloudinit
+done
+
+##Open the ports for the VMs:##
+for i in `seq 1 2 3 4` do
+    az vm open-port /
+    --port 8080 /
+    --resource-group $resourcegroup /
+    --name $vmname$i
 done
