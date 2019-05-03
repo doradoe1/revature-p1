e64d69e9e31235776a651d061561a279523a9070
doradoe1 <doradoe1@outlook.com>
2019-05-03 09:27:23 -0500

    Cleaned up folder structure

--- p1script.sh
+++ p1script.sh
@@ -2,10 +2,13 @@
 
 #-----------------------------------------------##Parameters.##------------------------------------------------#
 read -p "Enter the resource group name: " resourcegroup
+read -p "Enter the disk name: " diskname
+read -p "Enter the os type (Linux or Windows): " os
 read -p "Enter the name of the vm: " vmname
 read -p "Enter a name for the availability set: " as
 read -p "Enter the image (UbuntuLTS commonly used): " image
 read -p "Enter the size (Standard_B2s commonly used): " size
+read -p "Enter the size (read in gb) for the disk: "  sizegb
 read -p "Enter the custom data txt file: " cloudinit
 
 #--------------------------------------##Dummyproffing the new inputs:##--------------------------------------#
@@ -16,8 +19,18 @@ verifyrg=$(az group exists \
 if [ $verifyvrg=true ]; then
     exit 0
 else
-    echo "Invalid/non-existent Resource group. Please check your input and try again."
-    exit 1
+    echo "Invalid/non-existent Resource group. Please check your input and try again." 1>&2
+fi
+
+##Verify for valid disk name:##
+verifydisk=$(az disk list \
+    --resource-group $resourcegroup \
+    --query [].name \
+| grep -E $diskname)
+if [ -z $verifydisk ]; then
+    exit 0
+else
+    echo "Disk name already in use. Please choose another name for the disk and try again." 1>&2
 fi
 
 ##Verify for valid VM name:##
@@ -29,8 +42,7 @@ verifyvm=$(az vm list \
 if [ -z $verifyvm ]; then
     exit 0
 else
-    echo "VM name already in use. Please check your input and try again."
-    1>&2
+    echo "VM name already in use. Please check your input and try again." 1>&2
 fi
 
 #-------------------------------------##Create the VM's and the Avaib. Set:##----------------------------------#
@@ -50,7 +62,7 @@ for i in `seq 1 2 3 4`; do
     --size-gb $sizegb
 done
 
-##Create the VM:##
+##Create the VMs:##
 for i in `seq 1 2 3 4`; do
     az vm create \
     --resource-group $resourcegroup \
@@ -58,7 +70,9 @@ for i in `seq 1 2 3 4`; do
     --availability-set $as \
     --size $size \
     --image $image \
-    --disk-attach $diskname$i \
+    --attach-os-disk $diskname$i \
+    --admin-username user$i \
+    --generate-ssh-keys \
     --custom-data $cloudinit
 done
 
