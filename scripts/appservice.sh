2c434fbc793c48a4eee320cb0b0e94d366f99d7e
doradoe1 <doradoe1@outlook.com>
2019-05-06 11:25:23 -0500

    Database linked

--- scripts/appservice.sh
+++ scripts/appservice.sh
@@ -3,6 +3,10 @@
 ##Parameters:
 read -p "Enter the name for a new directory: " directory
 read -p "Enter the name for the app: " appname
+read -p "Enter a name for the mysql server: " server
+read -p "Enter the administrator username for the mysql server: " admin
+read -p "Enter a password for the mysql server (8 char. min): " password
+read -p "Enter a name for the mysql database: " mysql
 read -p "Enter the number of workers (3 to 4): " workers
 read -p "Enter a name for the scale set: " scaleset
 
@@ -16,11 +20,32 @@ cd revature-p1
 
 az webapp up -n $appname
 
+##Create a mysql database server:
+az mysql server create \
+--resource-group doradoe1_asp_Linux_centralus \
+--name $server \
+--admin-user $admin \
+--admin-password $password \
+--sku-name B_Gen5_1
+
+##Create a mysql database:
+az mysql db create \
+--name $mysql \
+--resource-group doradoe1_asp_Linux_centralus \
+--server-name $server
+
+##Link database to app:
+az group deployment create \
+--resource-group doradoe1_asp_Linux_centralus \
+--template-uri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-webapp-linux-managed-mysql/azuredeploy.json
+
+##Update the app service plan (Sets a defaul for the servers):
 az appservice plan update \
 --name $appname \
 --number-of-workers $workers \
 --resource-group doradoe1_asp_Linux_centralus
 
+##Create a scale set (allows for redundancy):
 az vmss create \
 --resource-group doradoe1_asp_Linux_centralus \
 --name $scaleset \
@@ -30,6 +55,7 @@ az vmss create \
 --admin-username $appname \
 --generate-ssh-keys
 
+##Create an autoscale rule:
 az monitor autoscale create \
 --resource-group doradoe1_asp_Linux_centralus \
 --resource $scaleset \
@@ -39,8 +65,9 @@ az monitor autoscale create \
 --max-count 4 \
 --count $workers
 
-sleep 5s
+sleep 10s
 
+##Display the information for the instances:
 az vmss list-instance-connection-info \
 --resource-group doradoe1_asp_Linux_centralus \
 --name $scaleset \
